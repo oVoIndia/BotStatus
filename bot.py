50ab32f13c53359d2a8ed9928ce26dc01aa6a779
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-28 21:40:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ async def BotzHub():
             day = dt.now(k).strftime("%d")
             year =  dt.now(k).strftime("%Y")
             t = dt.now(k).strftime("%H:%M:%S")
-            edit_text +=f"\n**𝙇𝙖𝙨𝙩 C𝙝𝙚𝙘𝙠𝙚𝙙 & U𝙥𝙙𝙖𝙩𝙚𝙙 𝙤𝙣:** \n`{t} - {day} {month} {year} [IST]`\n\n__Bots Status Are Auto-updated Every 1 hour__"
+            edit_text +=f"\n**Last Checked & Updated:** \n`{t} - {day} {month} {year} [IST]`\n\n__Bots Status Are Auto-updated Every 1 hour__"
             await user_bot.edit_message(int(chnl_id), msg_id, edit_text)
             print(f"Checks since last restart - {c}")
             print("Sleeping for 1 hours.")
